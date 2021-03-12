e8ee5104043a6efaea987cb7271821a8de5b1078
TDY <tdy034@163.com>
2021-03-12 09:54:59 +0800

    加入选择菜单

--- 9008tools-b/source.cpp
+++ 9008tools-b/source.cpp
@@ -27,15 +27,55 @@ void scanFile()
 	}
 
 	//计算分区大小
-	int b = buf[34]<<16;
-	cout << b;
+	//int b = buf[34]<<16;
+	int b = buf[42]<<16;
+	cout << b << endl;
 	system("pause");
 
 
 }
 
+void print()
+{
+		cout << "**********高通9008为您服务**********" << endl;
+		cout << "-->1 进入9008模式" << endl;
+		cout << "-->2 刷入引导文件" << endl;
+		cout << "-->3 选择芯片格式(默认UFS)" << endl;
+		cout << "-->4 回读GPT分区" << endl;
+		cout << "-->5 选择回读分区名并开始回读" << endl;
+		cout << "-->6 选择写入分区名并开始写入" << endl;
+		//cout << "7 进入9008模式" << endl;
+		//cout << "8 进入9008模式" << endl;
+		cout << "-->0 退出" << endl;
+
+}
+
+void chooseFun()
+{
+	int ch;
+	cout << "-->";
+	cin >> ch;
+	switch (ch)
+	{
+	case 1:
+		cout << "等待三秒，手机进入9008模式……"<<endl;
+		//---进入CMD，运行命令 adb reboot edl
+		system("adb devices");
+		system("adb reboot edl");
+		//-----------------------------------
+		print();
+		chooseFun();
+		break;
+	default:
+		break;
+	}
+}
+
+
 int main()
 {
-	scanFile();
+	//scanFile();
+	print();
+	chooseFun();
 	system("pause");
 }
